483211cf3fee8a11b7e5ff7f62dd209fbfda5c81
Wrxuan <2450341686@qq.com>
2022-09-26 09:22:46 +0800

    feat:登录完成

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,10 +1,24 @@
 // 实现对axios封装
-
-import axios from "axios"
+import axios from 'axios'
+import { Message } from 'element-ui'
 
 // 通过axios 创建 axios 实例
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
   timeout: 5000
+})
+
+// 拦截器（解决axios请求默认加data的问题）
+service.interceptors.response.use(response => {
+  const { success, message, data } = response.data
+  if (success) {
+    return data
+  } else {
+    Message.error(message)
+    return Promise.reject(new Error(message))
+  }
+}, error => {
+  Message.error(error.message)
+  return Promise.reject(error)
 })
 export default service
